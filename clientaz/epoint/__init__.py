05ab2d1647e657262e029e44c7c3ac355d9b5343
mmzeynalli <miradil.zeynalli@gmail.com>
2024-09-12 23:52:36 +0200

    Typo fix and warning message to Azerbaijani

--- clientaz/epoint/__init__.py
+++ clientaz/epoint/__init__.py
@@ -15,11 +15,14 @@ EPOINT_PRIVATE_KEY: str = os.getenv('EPOINT_PRIVATE_KEY', '')
 EPOINT_INTERFACE_LANG: str = os.getenv('EPOINT_INTERFACE_LANG', 'az')
 EPOINT_SUCCESS_REDIRECT_URL: Optional[str] = os.getenv('EPOINT_SUCCESS_REDIRECT_URL')
 EPOINT_FAILED_REDIRECT_URL: Optional[str] = os.getenv('EPOINT_FAILED_REDIRECT_URL')
-EPOINT_LOGGER_NAME = str = os.getenv('EPOINT_LOGGER_NAME', 'epoint')
+EPOINT_LOGGER_NAME: str = os.getenv('EPOINT_LOGGER_NAME', 'epoint')
 
 
 if not EPOINT_PUBLIC_KEY or not EPOINT_PRIVATE_KEY:
-    warn('set EPOINT_PUBLIC_KEY/EPOINT_PRIVATE_KEY env variables to use this library correctly')
+    warn(
+        'EPOINT_PUBLIC_KEY/EPOINT_PRIVATE_KEY mühit dəyişənlərinə dəyər verməsəniz'
+        'sorğular çalışmayacaq!'
+    )
 
 
 __all__ = [
