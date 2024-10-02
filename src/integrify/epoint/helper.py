9fbd7ef9f65132e127bab2fd468169306a12a49b
mmzeynalli <miradil.zeynalli@gmail.com>
2024-10-02 23:23:02 +0200

    Fixed poetry for windows in gh

--- src/integrify/epoint/helper.py
+++ src/integrify/epoint/helper.py
@@ -1,5 +1,7 @@
 import base64
 import json
+import sys
+from functools import partial
 from hashlib import sha1
 
 from integrify.epoint import env
@@ -7,10 +9,16 @@ from integrify.epoint.schemas.response import CallbackDataSchema, DecodedCallbac
 
 __all__ = ['generate_signature', 'decode_callback_data']
 
+# Python 3.8 support
+if sys.version_info >= (3, 9):
+    _sha1 = partial(sha1, usedforsecurity=False)
+else:
+    _sha1 = sha1
+
 
 def generate_signature(data: str) -> str:
     sgn_string = env.EPOINT_PRIVATE_KEY + data + env.EPOINT_PRIVATE_KEY
-    return base64.b64encode(sha1(sgn_string.encode(), usedforsecurity=False).digest()).decode()
+    return base64.b64encode(_sha1(string=sgn_string.encode()).digest()).decode()
 
 
 def decode_callback_data(data: CallbackDataSchema) -> DecodedCallbackDataSchema:
