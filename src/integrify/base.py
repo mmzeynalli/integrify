a7285162b49bf866a90ce48237856555b2242f11
Miradil Zeynalli <miradilz@lap5cd322frx8.se.axis.com>
2024-10-15 17:10:14 +0200

    Working on base structure

--- src/integrify/base.py
+++ src/integrify/base.py
@@ -1,10 +1,8 @@
-from typing import Any, Generic, Optional, TypeVar, Union
+from typing import Any, Generic, Optional, TypeVar, get_args
 from urllib.parse import urljoin
 
 import httpx
 from pydantic import BaseModel, Field
-
-from integrify.logger import LOGGER_FUNCTION
 
 ResponseType = TypeVar('ResponseType', bound=BaseModel)
 
@@ -23,76 +21,76 @@ class ApiResponse(BaseModel, Generic[ResponseType]):
     """Cavab sorğusunun body-si"""
 
 
-def send_request(func):
-    def wrapper(self, *args, **kwargs) -> ApiResponse[ResponseType]:
-        func(self, *args, **kwargs)
-        return self.__call__()
+class APISupport:
+    def __init__(self, name):
+        self.name = name
 
-    return wrapper
+        self.urls: dict[str, str] = {}  # endpoints
+        self.handlers: dict[str, APIHandler] = {}  # Handler for each endpoint
+
+        self.default_handler: Optional[APIHandler] = None
+        self.client = httpx.Client(timeout=10)
+
+    def add_url(self, route_name: str, url: str):
+        self.urls[route_name] = url
+
+    def set_default_handler(self, handler_class: type['APIHandler']):
+        self.default_handler = handler_class()
+
+    def add_handler(self, route_name: str, handler_class: type['APIHandler']):
+        self.handlers[route_name] = handler_class()
+
+    def __getattribute__(self, name: str) -> Any:
+        try:
+            return super().__getattribute__(name)
+        except AttributeError:
+            if name not in self.urls:
+                raise
+
+        assert self.base_url
+
+        url = urljoin(self.base_url, self.urls[name])
+        handler = self.handlers.get(name, self.default_handler)
+
+        return lambda *args, **kwds: self.req(url, handler, *args, **kwds)
+
+    # The next two functions are sync only
+    def req(self, url: str, handler: Optional['APIHandler'], *args, **kwds):
+        if handler:
+            data = handler.handle_request(*args, **kwds)
+
+        response = self.client.request('POST', url, json=data)
+
+        if handler:
+            return handler.handle_response(response)
+
+        return response
 
 
-class ApiRequest:
-    session: Union[httpx.Client, httpx.AsyncClient, None] = None
+class APIHandler(Generic[ResponseType]):
+    def __init__(self):
+        self.resp_model: type[ResponseType] = get_args(self.__class__.__orig_bases__[0])[0]  # type: ignore[attr-defined]
 
-    def __init__(self, client_name: str, logger_name: str):
-        self.base_url: str
-        self.path: str
-        self.verb: Optional[str] = None
-        self.headers: dict = {}
-        self.body: dict = {}
+    def get_headers(self):
+        return {}
 
-        #
-        self.client_name: str = client_name
-        self.logger = LOGGER_FUNCTION(logger_name)
+    def handle_request(self, *args, **kwds):
+        """Request payload-i Burda duzeldirik"""
+        raise NotImplementedError
 
-        self.resp_model: type[ResponseType]  # type: ignore[valid-type]
-        self.accept_status_codes: list[int] = [200, 201, 204]
-
-    @property
-    def url(self):
-        return urljoin(self.base_url, self.path)
-
-    def process_response(self, resp: httpx.Response):
+    def handle_response(self, resp: httpx.Response):
+        """Response-u Burda process edirik"""
         resp.body = resp.json()  # type: ignore[attr-defined]
 
-        if not resp.is_success:
-            self.logger.error(
-                f'{self.client_name} failed. Status code was {resp.status_code}. '
-                f'Content => {resp.body}'  # type: ignore[attr-defined]
-            )
-        elif resp.status_code not in self.accept_status_codes:
-            self.logger.error(
-                f'{self.client_name} response is not in {self.accept_status_codes}. '
-                f'Status: {resp.status_code}, body: {resp.body}'  # type: ignore[attr-defined]
-            )
+        # if not resp.is_success:
+        #     self.logger.error(
+        #         f'{self.client_name} failed. Status code was {resp.status_code}. '
+        #         f'Content => {resp.body}'  # type: ignore[attr-defined]
+        #     )
+        # elif resp.status_code not in self.accept_status_codes:
+        #     self.logger.error(
+        #         f'{self.client_name} response is not in {self.accept_status_codes}. '
+        #         f'Status: {resp.status_code}, body: {resp.body}'  # type: ignore[attr-defined]
+        #     )
 
         return ApiResponse[self.resp_model].model_validate(resp, from_attributes=True)  # type: ignore[name-defined]
-
-    def __call__(self, *args: Any, **kwds: Any): ...
-
-
-class SyncApiRequest(ApiRequest):
-    session: Optional[httpx.Client] = None
-
-    def __call__(self, *args: Any, **kwds: Any):
-        if not self.session:
-            self.session = httpx.Client(timeout=10)
-
-        resp = self.session.request(self.verb, self.url, data=self.body, headers=self.headers)  # type: ignore[arg-type]
-        return self.process_response(resp)
-
-
-class AsyncApiRequest(ApiRequest):
-    session: Optional[httpx.AsyncClient] = None
-
-    async def __call__(self, *args, **kwds):
-        if not self.session:
-            self.session = httpx.AsyncClient(timeout=10)
-
-        resp = await self.session.request(
-            self.verb,
-            self.url,
-            data=self.body,
-            headers=self.headers,
-        )
-        return self.process_response(resp)
