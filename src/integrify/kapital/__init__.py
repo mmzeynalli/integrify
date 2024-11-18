88941a1a1ece0a63a51f98f3384049b9d84d2b30
kazimovzaman2 <kazimovzaman2@gmail.com>
2024-11-18 15:27:39 +0400

    Added test card credentials for kapital

--- src/integrify/kapital/__init__.py
+++ src/integrify/kapital/__init__.py
@@ -2,6 +2,10 @@
 Dokumentasiya:
 
 EN: https://brawny-airport-7ca.notion.site/Kapital-bank-E-commerce-API-Documentation-6dd6a228c40644e3bef034bca7845e3c
+
+Development and testing üçün test kartı məlumatları:
+PAN: 4169741330151778, ExpDate: 11/26, CVV: 119
+PAN: 5239151747183468, ExpDate: 11/24, CVV2: 292
 """
 
 from .client import KapitalAsyncRequest, KapitalClientClass, KapitalRequest
