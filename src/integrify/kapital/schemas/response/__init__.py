0489c88efa2584819fd273b9c941d3043f810e16
kazimovzaman2 <kazimovzaman2@gmail.com>
2024-11-20 12:55:52 +0400

    Import everything inside of response init

--- src/integrify/kapital/schemas/response/__init__.py
+++ src/integrify/kapital/schemas/response/__init__.py
@@ -1,16 +1,5 @@
-from .base_response import BaseResponseSchema, ErrorResponseBodySchema  # noqa F401
-from .detailed_order_response import DetailedOrderInformationResponseSchema  # noqa F401
-from .order_response import (
-    CreateOrderResponseSchema,  # noqa F401
-    OrderInformationResponseSchema,  # noqa F401
-)
-from .refund_response import (
-    ClearingOrderResponseSchema,  # noqa F401
-    FullReverseOrderResponseSchema,  # noqa F401
-    PartialReverseOrderResponseSchema,  # noqa F401
-    RefundOrderResponseSchema,  # noqa F401
-)
-from .token_response import (
-    LinkCardTokenResponseSchema,  # noqa F401
-    ProcessPaymentWithSavedCardResponseSchema,  # noqa F401
-)
+from .base_response import *  # noqa F403
+from .detailed_order_response import *  # noqa F403
+from .order_response import *  # noqa F403
+from .refund_response import *  # noqa F403
+from .token_response import *  # noqa F403
