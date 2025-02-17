3b1a7868143903f377c20fbd6b3890787cd3f947
mmzeynalli <miradil.zeynalli@gmail.com>
2025-02-17 13:10:06 +0100

    Added new types to documentation

--- src/integrify/utils.py
+++ src/integrify/utils.py
@@ -9,10 +9,13 @@ _ResponseT = TypeVar('_ResponseT', bound=Union[BaseModel, dict])
 T = TypeVar('T')
 
 _UNSET = object()
-"""Unset arguments"""
+"""Set olunmamış argument dəyəri"""
 
 Unsettable = Union[T, Literal[_UNSET]]  # type: ignore[valid-type]
+""" Optional argument tipi """
+
 UnsettableAndNone = Union[T, Literal[_UNSET], None]  # type: ignore[valid-type]
+"""None dəyəri ala bilən optional argument tipi"""
 
 
 class Environment(str, Enum):
